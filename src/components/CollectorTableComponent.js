35e4d6c199f34e55aa9609de06290d8cf3208374
MrMilhas <danielmrezende@yahoo.com.br>
2023-12-15 18:21:00 -0300

    Implementando tabela de coletores

---
+++ src/components/CollectorTableComponent.js
@@ -0,0 +1,203 @@
+import { SearchOutlined } from '@ant-design/icons';
+import React, { useRef, useState } from 'react';
+import Highlighter from 'react-highlight-words';
+import { Button, Input, Space, Table, Radio, Divider, Tag } from 'antd';
+
+const FormComponent = ({ data }) => {
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const [selectionType, setSelectionType] = useState('checkbox');
+    const searchInput = useRef(null);
+
+    const rowSelection = {
+        onChange: (selectedRowKeys, selectedRows) => {
+        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+        },
+        getCheckboxProps: (record) => ({
+        disabled: record.name === 'Disabled User',
+        // Column configuration not to be checked
+        name: record.name,
+        }),
+    };
+
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText('');
+    };
+
+    const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+        <div
+            style={{
+            padding: 8,
+            }}
+            onKeyDown={(e) => e.stopPropagation()}
+        >
+            <Input
+            ref={searchInput}
+            placeholder={`Search ${dataIndex}`}
+            value={selectedKeys[0]}
+            onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+            onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            style={{
+                marginBottom: 8,
+                display: 'block',
+            }}
+            />
+            <Space>
+            <Button
+                type="primary"
+                onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                icon={<SearchOutlined />}
+                size="small"
+                style={{
+                width: 90,
+                }}
+            >
+                Search
+            </Button>
+            <Button
+                onClick={() => clearFilters && handleReset(clearFilters)}
+                size="small"
+                style={{
+                width: 90,
+                }}
+            >
+                Reset
+            </Button>
+            <Button
+                type="link"
+                size="small"
+                onClick={() => {
+                confirm({
+                    closeDropdown: false,
+                });
+                setSearchText(selectedKeys[0]);
+                setSearchedColumn(dataIndex);
+                }}
+            >
+                Filter
+            </Button>
+            <Button
+                type="link"
+                size="small"
+                onClick={() => {
+                close();
+                }}
+            >
+                close
+            </Button>
+            </Space>
+        </div>
+        ),
+        filterIcon: (filtered) => (
+        <SearchOutlined
+            style={{
+            color: filtered ? '#1677ff' : undefined,
+            }}
+        />
+        ),
+        onFilter: (value, record) =>
+        record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+        if (visible) {
+            setTimeout(() => searchInput.current?.select(), 100);
+        }
+        },
+        render: (text) =>
+        searchedColumn === dataIndex ? (
+            <Highlighter
+            highlightStyle={{
+                backgroundColor: '#ffc069',
+                padding: 0,
+            }}
+            searchWords={[searchText]}
+            autoEscape
+            textToHighlight={text ? text.toString() : ''}
+            />
+        ) : (
+            text
+        ),
+    });
+    const columns = [
+        {
+            key: 1,
+            title: 'id',
+            dataIndex: 'id',
+            key: 'id',
+            render: (text) => <a>{text}</a>,
+            ...getColumnSearchProps('id'),
+            sorter: {
+                compare: (a, b) => a.id - b.id,
+                multiple: 4,
+            },
+        },
+        {
+            key: 2,
+            title: 'local',
+            dataIndex: 'local',
+            key: 'local',
+            render: (text) => <a>{text}</a>,
+            ...getColumnSearchProps('local'),
+            sorter: {
+                compare: (a, b) => a.id - b.id,
+                multiple: 4,
+            },
+        },
+        {
+            key: 3,
+            title: 'Inicio',
+            dataIndex: 'date_init',
+            key: 'date_init',
+            render: (text) => <a>{text}</a>,
+            ...getColumnSearchProps('date_init'),
+            sorter: {
+                compare: (a, b) => a.id - b.id,
+                multiple: 4,
+            },
+        },
+        {
+            key: 4,
+            title: 'Termino',
+            dataIndex: 'date_end',
+            key: 'date_end',
+            render: (text) => <a>{text}</a>,
+            ...getColumnSearchProps('date_end'),
+            sorter: {
+                compare: (a, b) => a.id - b.id,
+                multiple: 4,
+            },
+        },
+        {
+            key: 5,
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+            <Space size="middle">
+                <a>Editar</a>
+                <a>Finalizar</a>
+            </Space>
+            ),
+        },
+    ]
+
+    return (
+        <div>
+            <Divider />
+
+            <Table 
+                columns = {columns}
+                dataSource = {data}
+            />
+        </div>
+    );
+
+};
+
+export default FormComponent;
--- src/pages/CollectorRegisterPage.js
+++ src/pages/CollectorRegisterPage.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect} from 'react';
 import { Modal, Button, Form, Select } from 'antd';
-import FormComponent from '../components/FormComponent.js';
+import FormComponent from '../components/CollectorTableComponent.js';
 
 const CollectorRegisterPage = () => {
+    const [data, setData] = useState([]);
+
+    function generateCollectorData() {
+        const id = Math.floor(Math.random() * 1000);
+        const date_init = new Date().toISOString();
+        const date_end = null;
+        const local = 'Sala 2305';
+
+        return {id, date_init, date_end, local};
+    }
+
+    useEffect(() => {
+        const collectorData = generateCollectorData();
+
+        setData((prevData) => [...prevData, { ...collectorData}]);
+    }, [data]);
+
     return (
         <div>
-            <FormComponent />
+            <FormComponent data={data}/>
         </div>
+
     );
 }
 
